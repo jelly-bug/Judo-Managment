cd0b894b1cdd0cf99e57297c75bebb7ce5282078
jelly-bug <ponokyleangelli@gmail.com>
2025-01-14 20:23:12 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'sigma'
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://vince:426999@localhost:5432/judo_management"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kyle:426999@localhost:5432/judo_management"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
